3535e75326104484a08d9a8a0962c61a6f5a0eaa
Morteza Bozorgzade <morza.bezrgzd@gmail.com>
2023-07-11 20:32:11 +0430

    start project english story

---
+++ android/app/src/main/kotlin/com/example/english_story/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.english_story
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants/app_routes.dart
@@ -0,0 +1,5 @@
+// ignore_for_file: constant_identifier_names
+
+class AppRouters {
+  static const String SPLASHROUTE = '/splash';
+}
